262757e26b4fa611db1e926c408d5d03a6a45ce5
Anurag1020 <anurag.mendhe14@gmail.com>
2022-06-15 21:10:50 +0530

    initial commit

--- loops/Program.cs
+++ loops/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Anurag!");
+
+
+for (int i = 10; i < 100; i++ )
+{
+    Console.WriteLine("i :" + i);
+}
